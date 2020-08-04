45f2b00d9b4ec35e4b8f3d34f7840a886ff15ecd
Shay Lewis <34757626+shaylew@users.noreply.github.com>
2020-08-04 13:46:09 -0400

    Updates

--- src/types/svg.d.ts
+++
@@ -1,4 +0,0 @@
-declare module '*.inline.svg' {
-  const content: import('react').FC<import('react').SVGAttributes<SVGElement>>;
-  export default content;
-}
--- src/types/typography-theme.d.ts
+++
@@ -1,5 +0,0 @@
-declare module 'typography-theme-wordpress-2016' {
-  import { TypographyOptions } from 'typography';
-  const Theme: TypographyOptions;
-  export = Theme;
-}
